d478a1c06396f11b14d06fa2f2fa831a8164bdaf
Yash7256 <amanrajengg@gmail.com>
2026-09-21 00:51:20 +0530

    Remove HAO Cabs and Shubh Motors client logos

--- src/data/clients.ts
+++ src/data/clients.ts
@@ -13,11 +13,9 @@ export const clients: Client[] = [
   { name: "Client 05", logo: "/Clients/5.png", width: 854, height: 764 },
   { name: "Client 06", logo: "/Clients/6.png", width: 858, height: 411 },
   { name: "Client 07", logo: "/Clients/7.png", width: 861, height: 332 },
-  { name: "Client 08", logo: "/Clients/8.png", width: 898, height: 114 },
   { name: "Client 09", logo: "/Clients/9.png", width: 864, height: 235 },
   { name: "Client 10", logo: "/Clients/10.png", width: 864, height: 250 },
   { name: "Client 11", logo: "/Clients/11.png", width: 851, height: 435 },
   { name: "Client 12", logo: "/Clients/12.png", width: 830, height: 830 },
   { name: "Client 13", logo: "/Clients/13.png", width: 749, height: 364 },
-  { name: "Client 14", logo: "/Clients/14.png", width: 795, height: 132 },
 ];
